0b2ba5ef34f83f2a5a6e9f60390b19f7d0e007f2
ojodaniel20101-prog <ojodaniel20101@gmail.com>
2026-05-18 16:44:51 +0100

    fix port and debug

--- movies_api.py
+++ movies_api.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 import requests
 from bs4 import BeautifulSoup
@@ -11,7 +12,6 @@ def get_poster_from_detail(link, headers):
         img = soup.find("img", class_="attachment-post-thumbnail")
         if img:
             return img.get("data-src") or img.get("src")
-        # fallback - get first image in post content
         content = soup.find("div", class_="entry-content")
         if content:
             img = content.find("img")
@@ -69,4 +69,5 @@ def movie_search():
     return jsonify(results)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
